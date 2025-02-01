05ec7b17f012144cd5f5742ac478780821fc14c0
FlowForecast <secondrage059@gmail.com>
2025-02-01 13:44:26 +0800

    Update dashScript.js

--- dashScript.js
+++ dashScript.js
@@ -436,8 +436,19 @@ document.addEventListener('DOMContentLoaded', () => {
     updateForecast(defaultCity);
     updateTrafficMap(defaultCity);
     fetchLocalEvents(defaultCity);
+});
 
-    locationButton.addEventListener('click', () => {
+/* ==================== Header Scroll Effect ==================== */
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Adjust the scroll position threshold as needed
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+locationButton.addEventListener('click', () => {
     // Fetch location using IPGeolocation API
     fetch('https://api.ipgeolocation.io/ipgeo?apiKey=b210b7b34c19429891fe3554d9a60476')
         .then(response => {
@@ -474,17 +485,4 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error during location retrieval:', error);
             alert('Failed to retrieve your location. Please try again.');
         });
-
 });
-
-/* ==================== Header Scroll Effect ==================== */
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) { // Adjust the scroll position threshold as needed
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
-
-
