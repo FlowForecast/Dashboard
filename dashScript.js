80da0ae06b5496d5eb2fa3b3fac5e961cdc31e43
FlowForecast <secondrage059@gmail.com>
2025-02-11 15:12:37 +0800

    Add files via upload

--- dashScript.js
+++ dashScript.js
@@ -526,3 +526,15 @@ window.addEventListener('scroll', function () {
         header.classList.remove('scrolled');
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.menu a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            document.querySelector(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
